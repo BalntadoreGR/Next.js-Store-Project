cbe64eb32f8d1031d6111ae471cac6f7f38d0675
BalntadoreGR <george.anast@hotmail.gr>
2025-03-14 19:29:12 +0200

    Some Fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,11 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Next Furniture Store",
-  description: "A store built with Next.js",
+  title: 'Next Storefront',
+  description: 'A nifty store built with Next.js',
 };
 
 export default function RootLayout({
@@ -23,12 +14,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang='en' suppressHydrationWarning>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
