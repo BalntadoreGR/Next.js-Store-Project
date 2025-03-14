28db30faef44e7bcb542e9a2878a76e3336db8bc
BalntadoreGR <george.anast@hotmail.gr>
2025-03-14 19:59:08 +0200

    Added Components and Container

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import {Button} from "@/components/ui/button"
 
-const HomePage = () => {
-  return <div>Homepage</div>;
+function HomePage(){
+
+  return <div>
+    <h1 className='text-3xl'>Homepage</h1>
+    <Button variant="outline" size="default" className='capitalize m-8'>click me</Button>
+    </div>
 };
 export default HomePage;
