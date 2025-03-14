c8b7a6350d62111195816e7f8210f5ceed430a62
BalntadoreGR <george.anast@hotmail.gr>
2025-03-15 00:15:42 +0200

    Navbar Bprint and Logo

--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { Button } from '../ui/button'
+import Link from 'next/link'
+import { RxColorWheel } from "react-icons/rx";
 
 function Logo() {
   return (
-    <div>
-      Logo
-    </div>
+    <Button size="icon" asChild>
+        <Link href='/'>
+            <RxColorWheel className='w-8 h-8'/>
+        </Link>
+    </Button>
   )
 }
 
