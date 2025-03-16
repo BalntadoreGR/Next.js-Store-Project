6f3f85b1f53d118d0b3101c37f8e2d0521917432
BalntadoreGR <george.anast@hotmail.gr>
2025-03-16 23:54:30 +0200

    Hero Comp and Products Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import {Button} from "@/components/ui/button"
 import Hero from '@/components/home/Hero';
 import FeaturedProducts from '@/components/home/FeaturedProducts';
+import LoadingContainer from '@/components/global/LoadingContainer';
 
 function HomePage(){
   return <>
     <Hero/>
-    <FeaturedProducts/>
+    <Suspense fallback={<LoadingContainer/>}>
+      <FeaturedProducts/>
+    </Suspense>
   </>
 };
 export default HomePage;
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,10 +1,11 @@
+import ProductsContainer from '@/components/products/ProductsContainer';
 import React from 'react'
 
-function ProductsPage() {
+function ProductsPage({searchParams} : {searchParams:{ layout?: string, search?: string }}) {
+  const layout = searchParams.layout || 'grid';
+  const search = searchParams.search || '';
   return (
-    <div>
-      ProductsPage
-    </div>
+    <ProductsContainer layout={layout} search={search}/>
   )
 }
 
